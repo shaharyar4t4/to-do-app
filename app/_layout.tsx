95bcc25695688f9938e8fa1a3bf35ff08334adbe
Shaharyar <shaharyar4t4@gmail.com>
2025-08-01 00:58:11 +0500

    Make a folder structure

---
+++ app/_layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import View_home from './screen/view_home'
+
+const _layout = () => {
+  return (
+    <View_home/>
+  )
+}
+
+export default _layout
---
+++ app/screen/view_home.tsx
@@ -0,0 +1,13 @@
+import MainLayout from '@/components/mainlayout'
+import React from 'react'
+
+const View_home = () => {
+  return (
+    <MainLayout>
+        
+    </MainLayout>
+        
+  )
+}
+
+export default View_home
